2c55dcf3f1377e07da10789196f897e4476990bd
Nikhil Desai <nikhil@drivemotors.com>
2018-08-27 23:43:43 -0700

    clean up

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,19 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.demo.manager.CampsiteManager;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-	
-	@Bean
-	CampsiteManager campsiteManager() {
-		return new CampsiteManager();
 	}
 }
