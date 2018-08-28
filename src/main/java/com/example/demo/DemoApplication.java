c214a43016edd468153d97fefa8b8f304e844c98
Nikhil Desai <nikhil@drivemotors.com>
2018-08-27 23:46:05 -0700

    add bean

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.manager.CampsiteManager;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	CampsiteManager campsiteManager() {
+		return new CampsiteManager();
 	}
 }
